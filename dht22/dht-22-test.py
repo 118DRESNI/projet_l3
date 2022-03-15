17ee3641fecf45e2e217a64c87e93fd5058c8dbd
118DRESNI <nicolasdressayre@hotmail.com>
2022-03-15 22:39:50 +0100

    lecture dht22

--- dht22/dht-22-test.py
+++ dht22/dht-22-test.py
@@ -1,37 +1,58 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
 import board
 import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT22(board.D18)
+# Déclaration d'un DHT22 sur la broche D17 du GPIO
+dhtDevice = adafruit_dht.DHT22(board.D17, use_pulseio=False)
+# La prériode d'échantillonnage du DHT22
+Tech = 3.0
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+# Creation du plot
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
 
-while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
+def lireTemp():
+        try:
+                temp = dhtDevice.temperature
+                #humi = dhtDevice.humidity
+        except RuntimeError as error:
+                print(error.args[0])
+                time.sleep(Tech)
+        except Exception as error:
+                dhtDevice.exit()
+                raise error
+        return temp
 
-    time.sleep(2.0)
+
+def animer(i, xs, ys):
+
+        # Effectuer une lecture
+        temp_c = lireTemp()
+
+        # Ajouter x et y aux listes
+        xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+        ys.append(temp_c)
+
+        # Limiter les listes x et y a 20 elements
+        xs = xs[-20:]
+        ys = ys[-20:]
+
+        # plotter les listes x et y 
+        ax.clear()
+        ax.plot(xs, ys)
+
+        # formattage du plot 
+        plt.xticks(rotation=45, ha='right')
+        plt.subplots_adjust(bottom=0.30)
+        plt.title('Température du DHT22')
+        plt.ylabel('Température (°C)')
+
+
+ani = animation.FuncAnimation(fig, animer, fargs=(xs, ys), interval=1000*Tech)
+plt.show()
