4f7907a5fd2f6e93721f820dd7d55282f07a37ae
118DRESNI <nicolasdressayre@hotmail.com>
2022-04-13 16:47:37 +0200

    ajout timestamps comme axe x

--- dht22/humidity-writedata.py
+++ dht22/humidity-writedata.py
@@ -4,17 +4,17 @@ import time
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
-x_value = 0
+
 #nom des colonnes
 fieldnames = ["temps", "temperature", "humidite"]
 
 
 #ouvrir le fichier csv pour append les nouvelles valeurs
-def ecrireLigne(humidity, temperature, x_value):
+def ecrireLigne(humidity, temperature, temps):
     with open('data.csv', 'a') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         info = {
-            "temps": x_value,
+            "temps": time,
             "temperature": temperature,
             "humidite": humidity
         }
@@ -28,14 +28,14 @@ with open('data.csv', 'w') as csv_file:
 while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
+    t = time.localtime();
+    current_time = time.strftime("%H:%M:%S",t)
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+        print("Time =% Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(current_time, temperature, humidity))
     else:
         print("Failed to retrieve data from humidity sensor") 
 
-    ecrireLigne(humidity, temperature, x_value)
+    ecrireLigne(humidity, temperature, current_time)
 
-    x_value += 1 
     
-    time.sleep(100)
+    time.sleep(0.5)
